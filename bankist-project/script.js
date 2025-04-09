b4ed3d0f33638a7c2b933220d5d0c4277e74fc35
RiccardoParrino <riccardoparrino1@gmail.com>
2025-04-09 15:32:28 +0200

    make: account js

---
+++ bankist-project/script.js
@@ -0,0 +1,20 @@
+const riccardo = {
+    username: "riccardo",
+    pin: "111111",
+    balance: 26000,
+    ops: [26000,-3000,500,2000,100,400]
+};
+
+const francesco = {
+    username: "francesco",
+    pin: "123456",
+    balance: 2000,
+    ops: [1000,-500,500,500,500]
+};
+
+const giovanna = {
+    username: "giovanna",
+    pin: "123455",
+    balance: 5000,
+    ops: [4000,-500,500,500,500]
+}
