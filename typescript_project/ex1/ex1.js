72f52e8714513fc14a63331c9a3dfda3218edcbf
RiccardoParrino <riccardoparrino1@gmail.com>
2025-04-23 11:35:51 +0200

    adding getters and setters

---
+++ typescript_project/ex1/ex1.js
@@ -0,0 +1,4 @@
+function add(i1, i2) {
+    return i1 + i2;
+}
+console.log(add(1, 1));
--- typescript_project/ex1/ex1.ts
+++ typescript_project/ex1/ex1.ts
@@ -10,6 +10,22 @@ class Person {
         this.name = name;
         this.surname = surname;
     }
+
+    get getName() {
+        return this.name;
+    }
+
+    get getSurname() {
+        return this.surname;
+    }
+
+    set setName(name:string) {
+        this.name = name;
+    }
+
+    set setSurname(surname:string) {
+        this.surname = surname;
+    }
 }
 
 class Student extends Person {
@@ -17,6 +33,30 @@ class Student extends Person {
 
     constructor (name: string, surname:string, code:string) {
         super(name, surname);
+        this.code = code;
+    }
+
+    get getName() {
+        return super.getName;
+    }
+
+    get getSurname() {
+        return super.getSurname;
+    }
+
+    get getCode () : string {
+        return this.code;
+    }
+
+    set setName(name:string) {
+        super.setName;
+    }
+
+    set setSurname(surname:string) {
+        super.setSurname
+    }
+
+    set setCode(code:string) {
         this.code = code;
     }
 }
