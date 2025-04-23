70fbe2144e9d85a40f6bca67e7c9c524d598f960
RiccardoParrino <riccardoparrino1@gmail.com>
2025-04-23 11:25:15 +0200

    constructor added

---
+++ typescript_project/ex1/ex1.ts
@@ -0,0 +1,13 @@
+function add(i1:number, i2:number):number {
+    return i1 + i2;
+}
+
+class Person {
+    name: string;
+    surname: string;
+
+    constructor ( name:string, surname:string ){
+        this.name = name;
+        this.surname = surname;
+    }
+}
