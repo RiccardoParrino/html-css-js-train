d3fc2cad2bf280f1017e35513628a114b66aa252
RiccardoParrino <riccardoparrino1@gmail.com>
2025-04-23 15:20:08 +0200

    Optional and Default parameters function example added

--- typescript_project/function/function.ts
+++ typescript_project/function/function.ts
@@ -8,8 +8,18 @@ function add (i1:number, i2:number):number {
 
 
 // Optional parameters function example 
-function add2(i1:number, i2 = 3): number {
+function add2(i1:number, i2?:number): number {
+    if (i2)
+        return i1 + i2;
+    else
+        return i1;
+}
+console.log(add2(1,2));
+
+
+
+// Default parameters function example
+function add3(i1:number, i2 = 3): number {
     return i1 + i2;
 }
-console.log(add2(1));
-
+console.log(add3(1));
