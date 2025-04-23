25d44cb57c1c93e62a2f36bde81b01ff5ab0272a
RiccardoParrino <riccardoparrino1@gmail.com>
2025-04-23 15:13:59 +0200

    optional parameters function example added

--- typescript_project/function/function.ts
+++ typescript_project/function/function.ts
@@ -2,6 +2,14 @@
 function add (i1:number, i2:number):number {
     return i1 + i2;
 }
-
 // function invoking example
-console.log(add(1,2));
+// console.log(add(1,2));
+
+
+
+// Optional parameters function example 
+function add2(i1:number, i2 = 3): number {
+    return i1 + i2;
+}
+console.log(add2(1));
+
