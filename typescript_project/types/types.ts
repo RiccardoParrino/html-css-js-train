248b4353a973a51db0a2b297131e5b9f56b029de
RiccardoParrino <riccardoparrino1@gmail.com>
2025-04-24 15:33:15 +0200

    various example in types added

--- typescript_project/types/types.ts
+++ typescript_project/types/types.ts
@@ -1,6 +1,40 @@
-// instanceof example
+// defining constructors
+function C() {}
+function D() {}
+
+const o = new C();
+
+// true, because: Object.getPrototypeOf(o) === C.prototype
+Object
+o instanceof C;
+
+// false, because D.prototype is nowhere in o's prototype chain
+o instanceof D;
+
+o instanceof Object; // true, because:
+C.prototype instanceof Object; // true
+
+// Re-assign `constructor.prototype`: you should
+// rarely do this in practice.
+C.prototype = {};
+const o2 = new C();
+
+o2 instanceof C; // true
+
+// false, because C.prototype is nowhere in
+// o's prototype chain anymore
+o instanceof C;
+
+D.prototype = new C(); // add C to [[Prototype]] linkage of D
+D.name = "temp";
+console.log(D.name);
+console.log(D.prototype)
+const o3 = new D();
+console.log(o3 instanceof D); // true
+console.log(o3 instanceof C); // true since C.prototype is now in o3's prototype chain
 
 // typeof example
-const i1:number = 10;
-console.log( typeof i1 );
-console.log( typeof typeof i1 );
+// const i1:number = 10;
+// console.log( typeof i1 );
+// console.log( typeof typeof i1 );
+
