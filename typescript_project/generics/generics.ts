595ee15869ff65c158d0edd4abe15d1c2ea93cea
RiccardoParrino <riccardoparrino1@gmail.com>
2025-04-24 12:09:12 +0200

    parametric interface example added

--- typescript_project/generics/generics.ts
+++ typescript_project/generics/generics.ts
@@ -19,3 +19,15 @@ console.log(myNumberSet);
 const myStringSet = new MyGenericSet<String>();
 myStringSet.add("temp1").add("temp2");
 console.log(myStringSet);
+
+
+// parametric interface
+interface Pair<K,V> {
+    myFunction(k:K) : V;
+}
+
+class Logarithm implements Pair<Number,Number>{
+    myFunction(k: Number): Number {
+        return Math.log(parseInt(k.toString()));
+    }
+}
