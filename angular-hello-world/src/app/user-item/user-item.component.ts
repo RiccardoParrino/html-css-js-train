de6ae22edc6a6f285705e115b7fcff412a58ab1a
RiccardoParrino <riccardoparrino1@gmail.com>
2025-05-05 16:19:59 +0200

    add component with property

--- angular-hello-world/src/app/user-item/user-item.component.ts
+++ angular-hello-world/src/app/user-item/user-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-item',
@@ -6,6 +6,12 @@ import { Component } from '@angular/core';
   templateUrl: './user-item.component.html',
   styleUrl: './user-item.component.css'
 })
-export class UserItemComponent {
+export class UserItemComponent implements OnInit {
+  name: string;
 
+  constructor() {
+    this.name = 'Felipe';
+  }
+
+  ngOnInit() {}
 }
